9c88d6d96397237bdc94cb1a424c27f29aae2582
Will Salas <129430083+willl182@users.noreply.github.com>
2024-10-07 14:27:34 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Desplazamiento suave al hacer clic en los enlaces del menú
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
